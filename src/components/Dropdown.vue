906d0a85275911edcc7d8251ffce14fa5e4d2a7e
Kyle Hatch <ky13h4tch@gmail.com>
2019-06-22 12:46:47 -0600

    updated dropshadow

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -83,7 +83,8 @@ export default {
   background-color: #424242;
   min-width: 160px;
   overflow: auto;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
   font-size: 14px;
   z-index: 99;
   border-radius: 4px;
